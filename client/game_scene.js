a27bf157a7b2c398d45f59da0d5ebb60caf741fd
TRCMKR <alexplay1224@gmail.com>
2025-05-11 10:54:08 +0300

    Update cursor control and add WASD movement

--- client/game_scene.js
+++ client/game_scene.js
@@ -18,7 +18,10 @@ export class GameScene extends Phaser.Scene {
     }
 
     create() {
+        this.pointer = this.input.activePointer;
+
         socket.emit('startGame', this.playerName);
+
         socket.once('join', (data) => {
             this.player = this.physics.add.image(data.x, data.y, "tank");
 
@@ -59,6 +62,13 @@ export class GameScene extends Phaser.Scene {
             this.cameras.main.startFollow(this.player);
             this.physics.world.setBounds(0, 0, worldWidth, worldHeight);
             this.cursors = this.input.keyboard.createCursorKeys();
+            this.wasd = this.input.keyboard.addKeys({
+                up: Phaser.Input.Keyboard.KeyCodes.W,
+                left: Phaser.Input.Keyboard.KeyCodes.A,
+                down: Phaser.Input.Keyboard.KeyCodes.S,
+                right: Phaser.Input.Keyboard.KeyCodes.D
+            });
+
 
             const minimap = this.cameras.add(0, 0, 500, 500).setName("minimap");
             const viewportWidth = this.scale.gameSize.width;
@@ -69,6 +79,8 @@ export class GameScene extends Phaser.Scene {
             minimap.setPosition(0, viewportHeight - minimap.height);
             minimap.setBackgroundColor(0xffffff);
             minimap.centerOn(worldWidth / 2, worldHeight / 2);
+
+            // this.pointer = this.input.activePointer.positionToCamera(this.cameras.main);
         });
 
         socket.on("currentPlayers", players => {
@@ -139,23 +151,38 @@ export class GameScene extends Phaser.Scene {
 
         const speed = 200;
 
-        if (this.cursors.left.isDown) {
+        const left = this.cursors.left.isDown || this.wasd.left.isDown;
+        const right = this.cursors.right.isDown || this.wasd.right.isDown;
+        const up = this.cursors.up.isDown || this.wasd.up.isDown;
+        const down = this.cursors.down.isDown || this.wasd.down.isDown;
+
+        this.player.setVelocity(0);
+
+        if (left) {
             this.player.setVelocityX(-speed);
-        } else if (this.cursors.right.isDown) {
+        } else if (right) {
             this.player.setVelocityX(speed);
-        } else {
-            this.player.setVelocityX(0);
         }
 
-        if (this.cursors.up.isDown) {
+        if (up) {
             this.player.setVelocityY(-speed);
-        } else if (this.cursors.down.isDown) {
+        } else if (down) {
             this.player.setVelocityY(speed);
-        } else {
-            this.player.setVelocityY(0);
         }
+
 
         this.player.nameText.setPosition(this.player.x, this.player.y - 50);
+
+        const worldPoint = this.input.activePointer.positionToCamera(this.cameras.main);
+
+        const angle = Phaser.Math.Angle.Between(
+            this.player.x, this.player.y,
+            worldPoint.x, worldPoint.y
+        );
+
+        // Update ray direction
+        this.player.rotation = angle;
+        this.player.ray.setAngle(angle);
 
         socket.emit("playerMovement", {
             x: this.player.x,
@@ -163,36 +190,28 @@ export class GameScene extends Phaser.Scene {
             rotation: this.player.rotation
         });
 
-        if (this.player.ray && this.player.rayGraphics) {
-            this.raycaster.removeMappedObjects(this.player);
-            this.player.ray.setOrigin(this.player.x, this.player.y);
+        this.raycaster.removeMappedObjects(this.player);
+        this.player.ray.setOrigin(this.player.x, this.player.y);
 
-            const pointer = this.input.activePointer;
-            const angle = Phaser.Math.Angle.Between(this.player.x, this.player.y, pointer.worldX, pointer.worldY);
+        const intersections = this.player.ray.castCone();
 
-            // Update ray direction
-            this.player.angle = angle;
-            this.player.ray.setAngle(angle);
+        this.player.rayGraphics.clear();
+        this.player.rayGraphics.fillStyle(0xFFFF00, 1);
 
-            const intersections = this.player.ray.castCone();
+        if (intersections.length > 0) {
+            const points = intersections.map(p => new Phaser.Math.Vector2(p.x, p.y));
+            points.unshift(new Phaser.Math.Vector2(this.player.x, this.player.y));
 
-            this.player.rayGraphics.clear();
-            this.player.rayGraphics.fillStyle(0xFFFF00, 1);
-
-            if (intersections.length > 0) {
-                const points = intersections.map(p => new Phaser.Math.Vector2(p.x, p.y));
-                points.unshift(new Phaser.Math.Vector2(this.player.x, this.player.y));
-
-                this.player.rayGraphics.beginPath();
-                this.player.rayGraphics.moveTo(points[0].x, points[0].y);
-                for (let i = 1; i < points.length; i++) {
-                    this.player.rayGraphics.lineTo(points[i].x, points[i].y);
-                }
-                this.player.rayGraphics.closePath();
-                this.player.rayGraphics.fillPath();
+            this.player.rayGraphics.beginPath();
+            this.player.rayGraphics.moveTo(points[0].x, points[0].y);
+            for (let i = 1; i < points.length; i++) {
+                this.player.rayGraphics.lineTo(points[i].x, points[i].y);
             }
-            this.raycaster.mapGameObjects(this.player, true);
+            this.player.rayGraphics.closePath();
+            this.player.rayGraphics.fillPath();
         }
+        this.raycaster.mapGameObjects(this.player, true);
+
 
         Object.values(otherPlayers).forEach((p) => {
             this.raycaster.removeMappedObjects(p);
